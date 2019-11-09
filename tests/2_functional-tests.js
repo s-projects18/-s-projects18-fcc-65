0e36a00a4af695d57511d4c791aa5d24b3dd6edb
Glitch (s-projects18-fcc-65) <none>
2019-11-09 07:34:32 +0000

    helmet-stuff / security-test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,7 +14,7 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+/*
   suite('API ROUTING FOR /api/threads/:board', function() {
     
     suite('POST', function() {
@@ -55,5 +55,24 @@ suite('Functional Tests', function() {
     });
     
   });
+*/  
 
-});
+  // extra-tests
+  suite('extra-tests', function() {
+    
+    // extra: suite security
+  	suite('GET / => check security', function() {
+      test('content-security-policy', function(done) {
+        chai.request(server)
+        .get('/api')
+        .end(function(err, res){ 
+           assert.equal(res.header['x-frame-options'], 'SAMEORIGIN');
+           assert.equal(res.header['x-dns-prefetch-control'], 'off');
+           assert.equal(res.header['referrer-policy'], 'same-origin');
+           done();
+        });         
+      });
+    });
+  });  // extra-tests
+  
+}); // suite: Functional Tests
